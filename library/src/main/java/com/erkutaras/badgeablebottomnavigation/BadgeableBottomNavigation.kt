628993e74316b8f7d425311da45cddd10bbacd4d
erkutaras <info@erkutaras.com>
2018-12-30 18:10:24 +0300

    create library and sample project

---
+++ library/src/main/java/com/erkutaras/badgeablebottomnavigation/BadgeableBottomNavigation.kt
@@ -0,0 +1,18 @@
+package com.erkutaras.badgeablebottomnavigation
+
+import android.content.Context
+import android.support.design.widget.BottomNavigationView
+import android.util.AttributeSet
+
+/**
+ * Created by erkutaras on 30.12.2018.
+ */
+class BadgeableBottomNavigation @JvmOverloads constructor(context: Context,
+                                                          attrs: AttributeSet? = null,
+                                                          defStyleAttr: Int = 0)
+    : BottomNavigationView(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+}
---
+++ sample/src/main/java/com/erkutaras/badgeablebottomnavigation/sample/BadgeableBottomNavigationSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.erkutaras.badgeablebottomnavigation.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BadgeableBottomNavigationSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_badgeable_bottom_navigation_sample)
+    }
+}
