29c190bb2506ca1cb9a4ef2c3861e874b6d9b5f2
erkutaras <info@erkutaras.com>
2019-03-06 00:20:19 +0300

    implement circle and number badges

--- library/src/main/java/com/erkutaras/badgeablebottomnavigation/BadgeableBottomNavigation.kt
+++ library/src/main/java/com/erkutaras/badgeablebottomnavigation/BadgeableBottomNavigation.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,9 +13,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 /**
  * Created by erkutaras on 30.12.2018.
  */
-class BadgeableBottomNavigation @JvmOverloads constructor(context: Context,
-                                                          attrs: AttributeSet? = null,
-                                                          defStyleAttr: Int = 0):
+class BadgeableBottomNavigation @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
     BottomNavigationView(context, attrs, defStyleAttr),
     BottomNavigationView.OnNavigationItemSelectedListener,
     BottomNavigationView.OnNavigationItemReselectedListener {
@@ -27,10 +30,14 @@ class BadgeableBottomNavigation @JvmOverloads constructor(context: Context,
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.BadgeableBottomNavigation,
-            0, 0).apply {
+            0, 0
+        ).apply {
 
             try {
-                badgeType = BadgeType.values()[getInteger(R.styleable.BadgeableBottomNavigation_badgeType,BadgeType.NONE.ordinal)]
+                badgeType = BadgeType.values()[getInteger(
+                    R.styleable.BadgeableBottomNavigation_badgeType,
+                    BadgeType.NONE.ordinal
+                )]
             } finally {
                 recycle()
             }
@@ -41,20 +48,19 @@ class BadgeableBottomNavigation @JvmOverloads constructor(context: Context,
         super.onFinishInflate()
 
         val bottomNavigationView = getChildAt(0) as BottomNavigationMenuView
-
         for (i in 0 until bottomNavigationView.childCount) {
             val tabItem = bottomNavigationView.getChildAt(i) as BottomNavigationItemView
             tabList.add(tabItem.id)
-            when(badgeType) {
-                BadgeType.DOT -> {
-                    val badgeView = provideDotView(bottomNavigationView)
-                    badgeList.add(badgeView)
-                    tabItem.addView(badgeView)
-                }
-                BadgeType.NUMBER -> TODO()
-                BadgeType.CIRCLE -> TODO()
+
+            val badgeView: View = when (badgeType) {
+                BadgeType.DOT -> provideDotView(bottomNavigationView)
+                BadgeType.NUMBER -> provideNumberView(bottomNavigationView)
+                    .apply { findViewById<TextView>(R.id.textViewBadgeableNumber).text = i.toString() }
+                BadgeType.CIRCLE -> provideCircleView(bottomNavigationView)
                 BadgeType.NONE -> TODO()
             }
+            badgeList.add(badgeView)
+            tabItem.addView(badgeView)
         }
 
         badgeList[0].visibility = View.GONE
@@ -64,6 +70,12 @@ class BadgeableBottomNavigation @JvmOverloads constructor(context: Context,
 
     private fun provideDotView(bottomNavigationView: BottomNavigationMenuView): View =
         LayoutInflater.from(context).inflate(R.layout.layout_badgeable_dot, bottomNavigationView, false)
+
+    private fun provideNumberView(bottomNavigationView: BottomNavigationMenuView): View =
+        LayoutInflater.from(context).inflate(R.layout.layout_badgeable_number, bottomNavigationView, false)
+
+    private fun provideCircleView(bottomNavigationView: BottomNavigationMenuView): View =
+        LayoutInflater.from(context).inflate(R.layout.layout_badgeable_circle, bottomNavigationView, false)
 
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         updateBadgeVisibility(menuItem)
